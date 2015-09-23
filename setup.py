0a97b143e0885cea325ceec501fa75ea1cea2ee7
Gregor Sturm <mail@gregor-sturm.de>
2015-09-23 12:09:05 +0200

    converted the notebook to module

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name='nbimporter',
+      version='0.1',
+      description='Import IPython notebooks as modules',
+      author='Gregor Sturm',
+      author_email='mail@gregor-sturm.de',
+      py_modules=['nbimporter', 'display_nb'],
+      url='https://github.com/grst/nbimporter',
+ )
